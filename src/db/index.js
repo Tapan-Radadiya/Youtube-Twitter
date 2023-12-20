c338d1806af1121b3b546566594f711914c56253
Tapan Radadiya <84001495+Tapan-Radadiya@users.noreply.github.com>
2023-12-20 19:20:45 +0530

    DB index changes

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ async function connectDB() {
         const connectionProcess = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log("mongodb Connected", connectionProcess.connection.host);
     } catch (error) {
-        console.error("Error : !!", error)
+        console.error("MongoDb Connecction Failed : !!", error)
         process.exit(1)
     }
 }
