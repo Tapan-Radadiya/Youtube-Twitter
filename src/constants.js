93c49f7701a64ac7a0b28dac58f558ab6f2c86ce
Tapan Radadiya <84001495+Tapan-Radadiya@users.noreply.github.com>
2023-12-20 19:15:56 +0530

    Changes

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = "Tapan"
+export const DB_NAME = "Tapan123"
--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js"
 async function connectDB() {
     try {
         const connectionProcess = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log(connectionProcess.connection.host);
+        console.log("mongodb Connected", connectionProcess.connection.host);
     } catch (error) {
         console.error("Error : !!", error)
         process.exit(1)
